f5570c8b0e415a6e5830e4582e2ba1ceef473c3c
Mojo Jojo <mojo@jojo.com>
2025-07-17 10:07:38 +0000

    added exercise 9

---
+++ Intro_JS_Book/The_Basics/exercise_9.js
@@ -0,0 +1,4 @@
+// Create an object named pets that contains a list of pet names and the type of animal.
+
+let pets = {Hugo : 'dog', Coco :'dog', Bella : 'cat', Arya : 'parrot', Pudding : 'hamster'};
+console.log(pets);
