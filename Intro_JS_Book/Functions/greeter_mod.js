277958459cd6fcbb1d0581493e6a340f995670d6
Mojo Jojo <mojo@jojo.com>
2025-07-20 13:14:25 +0000

    ammended greeter

--- Intro_JS_Book/Functions/greeter_mod.js
+++ Intro_JS_Book/Functions/greeter_mod.js
@@ -17,21 +17,25 @@ firstLast();
 
 // ---------------------------------------------------------------------------------------------------------
 /*
+I did not use the hint
 This is a second approach where the function does return the two inputs which are captured outside the block
 thus making the first and last names accessible to console.log outside the function. 
 However I had to google how to return more than one value (ans: use container like arr or obj). The I had to 
-also google how to modify the arr into a string and remove the comma. 
+also google how to pull values from the object. 
 Overall this was a great learning oportunity !
 */
 
 let rlSync = require('readline-sync');
-function firstLast(first, last) {
+function getFirstLastName() {
    first = rlSync.question('What is your first name?  ');
    last = rlSync.question('What is your last name?  ');
   //console.log(`Greetings ${first} ${last}!`);
-  return [first, last]; 
+  return {firstName : first, lastName : last}; 
   
 }
-let fullName = firstLast();
-console.log(`Greetings ${(fullName.toString()).replaceAll(",", " ")}!`);
+let fullName = getFirstLastName();
+console.log(`Greetings ${fullName.firstName} ${fullName.lastName}!`);
+
+// after seeing the hint I understand that I wrote a one time script whyle the solution is more reusable
+// and elegant. Much to learn...
 
