82d926cb6cac4604ab7f3d819b156f871260fbfb
Mojo Jojo <mojo@jojo.com>
2025-07-21 09:44:27 +0000

    forgot to save it

---
+++ Intro_JS_Book/Functions/exercise_4.js
@@ -0,0 +1,13 @@
+/*
+  function scream(words) {
+  words = words + '!!!!';
+  return;
+  console.log(words);
+}
+
+scream('Yipeee');
+
+ANS: nothing will be printed because conso.log comes after return which exits the function
+     before console.log gets the chance to display output.
+
+*/
