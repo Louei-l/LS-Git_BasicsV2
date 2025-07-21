b2d03cc16f52e97007a7f43b2dff7f74208f8ddc
Mojo Jojo <mojo@jojo.com>
2025-07-21 10:28:33 +0000

    added exercise 8

---
+++ Intro_JS_Book/Functions/exercise_8.js
@@ -0,0 +1,14 @@
+/*
+function foo(bar, qux) {
+  console.log(bar);
+  console.log(qux);
+}
+
+foo(42, 3.1415, 2.718);
+
+ANS: we get 
+     42
+     3.1415
+
+     last argument is not printed because function accepts only 2 parameters.
+*/
