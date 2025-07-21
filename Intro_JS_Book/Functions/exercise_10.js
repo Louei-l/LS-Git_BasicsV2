cebab14688fbdb524b71bde5bc9d118dffe5949a
Mojo Jojo <mojo@jojo.com>
2025-07-21 10:55:57 +0000

    added exercise 10

---
+++ Intro_JS_Book/Functions/exercise_10.js
@@ -0,0 +1,21 @@
+/*
+function multiply(left, right) {
+  let product = left * right;
+  return product;
+}
+
+function getNumber(prompt) {
+  return parseFloat(question(prompt));
+}
+
+let left = getNumber('Enter the first number: ');
+let right = getNumber('Enter the second number: ');
+console.log(`${left} * ${right} = ${multiply(left, right)}`);
+
+
+ANS:  global varibales => multiply, left, right, getNumber
+      local variables => product
+Learning: forgot about console being a variable, it is global also. Didnt know that left, right can be both
+          globgal and local variables.
+     
+*/
