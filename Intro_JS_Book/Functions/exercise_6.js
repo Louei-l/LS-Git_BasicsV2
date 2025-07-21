2ce3292474d7ab757c23ebbba4714ca4ebe01ac3
Mojo Jojo <mojo@jojo.com>
2025-07-21 09:41:21 +0000

    added exercise 6

---
+++ Intro_JS_Book/Functions/exercise_6.js
@@ -0,0 +1,42 @@
+/*
+  In the code shown below, identify the following items:
+
+the function arguments
+the function body
+the function declaration
+the function invocation
+the function name
+the function parameters
+the function return value
+the names of all variables in this program
+
+
+function multiplyNumbers(num1, num2, num3) {
+  let result = num1 * num2 * num3;
+  return result;
+}
+
+let product = multiplyNumbers(2, 3, 4);
+
+
+the function arguments => 2, 3, 4
+
+the function body ==> let result = num1 * num2 * num3;
+                      return result;
+
+the function declaration ==> function multiplyNumbers
+
+the function invocation ==> multiplyNumbers(2, 3, 4)
+
+the function name ==> multiplyNumbers
+
+the function parameters = num1, num2, num3
+
+the function return value ==> 24
+
+the names of all variables in this program ==> multiplyNumbers, num1, num2, num3, result, product
+
+got the function declaration wrong, I thought it is the same as variable declaration but it is not. 
+
+
+*/
