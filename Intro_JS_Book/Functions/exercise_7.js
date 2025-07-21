ad4fbbfc325d299c116a09e835ba39067131b51a
Mojo Jojo <mojo@jojo.com>
2025-07-21 10:25:56 +0000

    added exercise 7

---
+++ Intro_JS_Book/Functions/exercise_7.js
@@ -0,0 +1,14 @@
+
+
+/*
+function foo(bar, qux) {
+  console.log(bar);
+  console.log(qux);
+}
+
+foo('Hello');
+
+ANS: we get 
+     Hello
+     undefined <= because second argument is missing to qux becomes undefined
+*/
