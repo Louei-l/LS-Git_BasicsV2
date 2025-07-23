3e24a20982f084100d77a5aa315997fca958437d
Mojo Jojo <mojo@jojo.com>
2025-07-23 11:01:07 +0000

    added exercise 2

---
+++ Intro_JS_Book/Flow_Control/even_odd.js
@@ -0,0 +1,13 @@
+// attempt without hints
+
+function evenOrOdd (number) {
+  if (number % 2 === 0) {
+    console.log(`${number} is even`)
+  } else {
+    console.log(`${number} is odd`)
+  }
+}
+
+evenOrOdd(6);
+
+// happy it is exactly as soluction, without hints !
