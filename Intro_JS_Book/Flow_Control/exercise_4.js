8028ee487e855c5ccb363eeca2b4657ef8341c15
Mojo Jojo <mojo@jojo.com>
2025-07-23 13:19:38 +0000

    added exercise 4

---
+++ Intro_JS_Book/Flow_Control/exercise_4.js
@@ -0,0 +1,21 @@
+/* 
+What does the following code log to the console, and why?
+
+function barCodeScanner(serial) {
+  switch (serial) {
+    case '123':
+      console.log('Product1');
+    case '113':
+      console.log('Product2');
+    case '142':
+      console.log('Product3');
+    default:
+      console.log('Product not found!');
+  }
+}
+
+barCodeScanner('113'); // => Product 2
+
+Learning: forogt about break statement so the output will be all console.logs except for Product 1
+
+*/
