440c3686f4c97559902452fe72c49bec9ed48a1a
Mojo Jojo <mojo@jojo.com>
2025-07-23 13:56:05 +0000

    added exercise 6

---
+++ Intro_JS_Book/Flow_Control/exercise_6.js
@@ -0,0 +1,17 @@
+/* What does this code output to the console?
+
+  function isArrayEmpty(arr) {
+    if(arr){
+      console.log('Not Empty');
+    }
+  } else {
+      console.log('Empty');
+    }
+
+  isArrayEmpty([]); // => Not Empty
+  
+  Ans: if statement evaluates only the truthiness state. 
+  Only 0, NaN, undefined, null, -0, false values are falsy anythins else is true
+  even an [] empty array.
+  
+*/
