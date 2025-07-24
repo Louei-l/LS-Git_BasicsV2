992d7042213c3db579a195e23dcfe77e23e09495
Mojo Jojo <mojo@jojo.com>
2025-07-24 05:27:36 +0000

    added exercise 9

--- Intro_JS_Book/Flow_Control/exercise_9.js
+++ Intro_JS_Book/Flow_Control/exercise_9.js
@@ -1,11 +1,28 @@
-function intRange(int) {
-  if(int >= 0 && int <= 50) {
-    console.log(`${int} is between 0 and 50`);
-  } else if(int >= 51 && int <= 100) {
-      console.log(`${int} is between 51 and 100`);
-  } else if(int > 100 ) {
-      console.log(`${int} greater than 100`);
-  } else {
-      console.log(`${int} less than 0`);
-  }
-}
+/*
+console.log(false ?? null); ==> false, nullish coalescing operator evaluates right-hand operand if the left-hand one is nullish (null or undefined) 
+                                otherwise it evaluates to the value of the left-hand operand
+
+console.log(true ?? (1 + 2)); ==> true, neither of the operands are nullish (null/undefined),
+                                  retunrs left-hand operand
+
+console.log((1 + 2) ?? true); ==> 3, neither of the operands are nullish (null/undefined),
+                                  retunrs left-hand operand
+
+console.log(null ?? false); ==> false, left-hand operand is nullish (null/undefined), 
+                                hence it returns right hand operand
+
+console.log(undefined ?? (1 + 2)); ==> 3, left-hand operand is nullish (null/undefined), 
+                                hence it returns right hand operand
+
+console.log((1 + 2) ?? null); ==> null, left-hand operand is not nullish (null/undefined), 
+                                hence it returns right hand operand
+
+                               xx ==> wrong if left-hand is not nullish it return left hand
+
+console.log(null ?? undefined);  ==> undefined, left-hand operand is nullish (null/undefined), 
+                                hence it returns right hand operand
+
+console.log(undefined ?? null);  ==> null, left-hand operand is nullish (null/undefined), 
+                                hence it returns right hand operand
+
+*/
