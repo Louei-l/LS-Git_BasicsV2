39ab60cc4bb22b01ee28f79ab939302de87df8af
Mojo Jojo <mojo@jojo.com>
2025-09-28 05:54:27 +0000

    added 4th exercise

---
+++ JS100_Exercises/Reading_Documentation_1/Array_Element_Access.js
@@ -0,0 +1,5 @@
+/*
+the documentation about array object on MDN can be found here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+to access the 'and'element in the array ['fish', 'and', 'chips'] we can use several approaches we can simply write ['fish', 'and', 'chips'][1]
+or we can save the array ['fish', 'and', 'chips'] under arr variable and then access the element --> arr[1]
+*/
