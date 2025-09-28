d42c6f2f156f5cac773318f238eefcfaea1b44e3
Mojo Jojo <mojo@jojo.com>
2025-09-28 03:45:49 +0000

    added 3rd exercise

---
+++ JS100_Exercises/Reading_Documentation_1/mountain_caps.js
@@ -0,0 +1,5 @@
+/*
+there is no specific method to capitalize only one leter of a string. 
+a work around has to be done combining several methods for example str.charAt(0).toUpperCase() + str.slice(1)
+this will effectively capitalize the first character and then add the remaining of the string from 2nd character onwards
+*/
