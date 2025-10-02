9ed50baa0c5d2e344c9ada759d76a5c4f10829a5
Mojo Jojo <mojo@jojo.com>
2025-10-02 09:36:48 +0000

    added 6th exercise

---
+++ JS100_Exercises/Reading_Documentation_1/property_vs_method.js
@@ -0,0 +1,5 @@
+let trees = ['birch', 'pine', 'sequoia', 'palm tree'];
+
+trees[trees.length - 1]; // -> 'palm tree'
+trees.pop(); // -> removes the last element in the array
+trees[trees.length - 1]; // -> 'sequoia'
