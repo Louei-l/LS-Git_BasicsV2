740e0f1e8aa1d9f873b78032e0c789fa7760576f
Mojo Jojo <mojo@jojo.com>
2025-10-03 11:18:27 +0000

    equality.js

---
+++ JS100_Exercises/Reading_Documentation_1/equality.js
@@ -0,0 +1,4 @@
+// > '8' == 8; // -> == is a loose comparison it will convert different types into same so the string '8' will become number 8 and then the check will be performed 8==8 true
+true
+// > '8' === 8; // -> this is a strict comparis and gives false because the types do not match, so the output is false
+false
