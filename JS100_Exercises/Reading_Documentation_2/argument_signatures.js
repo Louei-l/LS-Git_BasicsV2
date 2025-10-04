78f13e0ceeb100a8c585189bb019baf4fe0a59b3
Mojo Jojo <mojo@jojo.com>
2025-10-04 06:27:31 +0000

    added 7th exercise

---
+++ JS100_Exercises/Reading_Documentation_2/argument_signatures.js
@@ -0,0 +1,5 @@
+/**
+ * the Array.prototype.join() method expects one argument that specifies the separator string.
+ * In case if no argument is given, the method will default to ',' as separator.
+ * In case if two or more arguments are provided, only the first one will be considered and the rest will be ignored.
+ */
