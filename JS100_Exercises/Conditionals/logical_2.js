00aaed41f493ff020e55380ba48b773a935975fa
Mojo Jojo <mojo@jojo.com>
2025-10-13 06:15:03 +0000

    added 8th exercise

---
+++ JS100_Exercises/Conditionals/logical_2.js
@@ -0,0 +1,9 @@
+if (true && false) {
+  console.log('Yes!');
+} else {
+  console.log('No...');
+}
+
+//the output will be No...
+//the condition provided to our if statement uses logical operator && (and). 
+//therefore the condition will evaluate to falsy since true and false produces false.
