715d66220c7d9e023a4da8e37f895b5aa4730c8f
Mojo Jojo <mojo@jojo.com>
2025-10-13 06:07:43 +0000

    added 7th exercise

---
+++ JS100_Exercises/Conditionals/logical_1.js
@@ -0,0 +1,8 @@
+if (false || true) {
+  console.log('Yes!');
+} else {
+  console.log('No...');
+}
+
+// the output will be Yes! only
+// because of the or condition either scenario false or true will alway satisfy the check
