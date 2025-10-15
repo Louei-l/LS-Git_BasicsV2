6920c1fbbeb39217bb6ce1522bd13f906c7273eb
Mojo Jojo <mojo@jojo.com>
2025-10-15 18:17:16 +0000

    added 5th exercise.

---
+++ JS100_Exercises/Functions_1/display_division.js
@@ -0,0 +1,23 @@
+function multiplesOfThree() {
+  let divisor = 1;
+
+  for (let dividend = 3; dividend <= 30; dividend += 3) {
+    console.log(dividend + ' / ' + divisor + ' = 3');
+    divisor += 1;
+  }
+}
+
+multiplesOfThree(); // => without parenthesis the code wont work as it will not invoke the function.
+                   // => so this could be a hidden issue that we had to notice. After fixing it it logs below stuff
+
+// logs:
+// 3 / 1 = 3
+// 6 / 2 = 3
+// 9 / 3 = 3
+// 12 / 4 = 3
+// 15 / 5 = 3
+// 18 / 6 = 3
+// 21 / 7 = 3
+// 24 / 8 = 3
+// 27 / 9 = 3
+// 30 / 10 = 3
