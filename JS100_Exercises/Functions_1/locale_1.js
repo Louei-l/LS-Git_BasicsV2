e0bb60af523b7e8c615cb7776e6d0efd1f013a72
Mojo Jojo <mojo@jojo.com>
2025-10-19 12:43:08 +0000

    amended 9th exercise.

--- JS100_Exercises/Functions_1/locale_1.js
+++ JS100_Exercises/Functions_1/locale_1.js
@@ -1,5 +1,5 @@
 const extractLanguage = (str) => {
-  return typeof str === 'string' ? str.slice(0, 2) : 'enter a string please';
+  return typeof str === 'string' ? str.split("_")[0] : 'enter a string please';
 }
 
 console.log(extractLanguage('en_US.UTF-8'));
