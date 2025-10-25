887fb31e932e0bbe0960d7e06e714b0e7face8fa
Mojo Jojo <mojo@jojo.com>
2025-10-25 11:14:32 +0000

    added 1st exercise.

---
+++ JS100_Exercises/Variable_Scope/whats_my_value_1.js
@@ -0,0 +1,8 @@
+if (true) {
+  let myValue = 20;
+}
+
+console.log(myValue); // => undefined.
+
+// myValue variable is decalred inside the function.
+// It is limited to the scope of the function and cannot be accessed from outer scope. 
