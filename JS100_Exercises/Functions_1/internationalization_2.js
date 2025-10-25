3017cb315320b4d8c4489ba73b5ab78f6dce2309
Mojo Jojo <mojo@jojo.com>
2025-10-25 11:03:17 +0000

    refactored 11th exercise.

--- JS100_Exercises/Functions_1/internationalization_2.js
+++ JS100_Exercises/Functions_1/internationalization_2.js
@@ -1,10 +1,16 @@
 const localGreeting = {
-  fr_FR: 'Salut!',
-  fr_CA: 'Salut!',
-  fr_MA: 'Salut!',
   en_US: 'Hey!',
   en_GB: 'Hello!',
   en_AU: 'Howdy!',
+}
+
+const langGreeting = {
+  fr: 'Salut!',
+  en: 'Hi!',
+  pt: 'Olá!',
+  de: 'Hallo!',
+  sv: 'Hej!',
+  af: 'Haai!',
 }
 
 
@@ -13,9 +19,25 @@ const localGreeting = {
 function localGreet(locale){
   const region = extractRegion(locale);
   const language = extractLanguage(locale);
-  const langRegion = String(language + '_' + region);
 
-  return greet(langRegion);
+  if (!language) {
+   return 'Invalid Locale';
+  }
+
+  const lang = language.toLowerCase();
+  const reg = region ? region.toUpperCase() : null;
+
+  if (reg) {
+    const key = `${lang}_${reg}`;
+    return 
+  }
+
+
+  if (localGreeting.hasOwnProperty(key)) {
+    return localGreeting[key]
+  }
+
+  return greet(lang) ?? 'Hello.';
 
 }
 
@@ -34,18 +56,22 @@ function extractRegion(locale)  {
 }
 
 function isValidLocale(input) {
-    let regex = /[A-Za-z]{2}_[A-Za-z]{2}.UTF-\d+$/;
+    let regex = /^[A-Za-z]{2}_[A-Za-z]{2}\.UTF-\d+$/;
     return regex.test(input);
 }
 
 function extractLanguage(str) {
-  return typeof str === 'string' ? str.split("_")[0] : 'enter a string please';
+  return typeof str === 'string' 
+  ? str.split("_")[0] 
+  : null;
 }
 
 
 
 function greet(lang) {
-  return localGreeting[lang];
+  return langGreeting.hasOwnProperty(lang) 
+  ? langGreeting[lang]
+  : null;
 }
 
 
