571885a4eabb119d1adb3e954b45c5e643c6256b
Mojo Jojo <mojo@jojo.com>
2025-10-25 11:17:23 +0000

    refined the answer

--- JS100_Exercises/Variable_Scope/whats_my_value_1.js
+++ JS100_Exercises/Variable_Scope/whats_my_value_1.js
@@ -2,7 +2,7 @@ if (true) {
   let myValue = 20;
 }
 
-console.log(myValue); // => undefined.
+console.log(myValue); // => error.
 
-// myValue variable is decalred inside the function.
-// It is limited to the scope of the function and cannot be accessed from outer scope. 
+// myValue variable is declared inside the block.
+// It is limited to the scope of the block and cannot be accessed from outer scope. 
