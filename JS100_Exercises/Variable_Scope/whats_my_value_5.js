57e3e885cce7f2058e68f7de3321b1055ad661e7
Mojo Jojo <mojo@jojo.com>
2025-10-26 07:36:24 +0000

    added 5th exercise.

---
+++ JS100_Exercises/Variable_Scope/whats_my_value_5.js
@@ -0,0 +1,14 @@
+let a = 1;
+
+function myFunction(a) {
+  console.log(a);
+}
+
+let b = 2;
+
+myFunction(b); // => 2
+
+// the a parameter is a placeholder to pass data to the function.
+// console.log log the data that is passed to the function's parameter. 
+// when myFunction is called it is called with variable b which has a value of 2. 
+// so 2 will be passed to the function and logged.
