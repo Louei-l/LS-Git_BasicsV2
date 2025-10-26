c310b3e697f9e38e084c2d2aaf0417d999daee2a
Mojo Jojo <mojo@jojo.com>
2025-10-26 06:28:42 +0000

    added 2nd exercise.

---
+++ JS100_Exercises/Variable_Scope/whats_my_value_2.js
@@ -0,0 +1,11 @@
+function myFunction() {
+  let a = 1;
+
+  if (true) {
+    console.log(a);
+  }
+}
+
+myFunction(); // => 1
+
+//the call to myFunction will output a since if check is always true in this case. 
