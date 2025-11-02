4416ce273bda3689cd62da505d66b595838c5dfb
Mojo Jojo <mojo@jojo.com>
2025-11-02 09:36:41 +0000

    added 1st exercise.

---
+++ JS100_Exercises/Strings/length.js
@@ -0,0 +1,5 @@
+// Determine the length of the string "These aren't the droids you're looking for.".
+
+const droids = "These aren't the droids you're looking for."
+
+console.log(droids.length); // => 43
