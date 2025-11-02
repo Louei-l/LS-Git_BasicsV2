bd991fe5098ba7135232794bdcf79c332e1952f3
Mojo Jojo <mojo@jojo.com>
2025-11-02 10:58:25 +0000

    added 5th exercise.

---
+++ JS100_Exercises/Strings/case_ins_equality.js
@@ -0,0 +1,8 @@
+let string1 = 'Polar Bear';
+let string2 = 'Polar bear';
+let string3 = 'Penguin';
+
+const areEqual1 = string1.toLowerCase() === string2.toLowerCase();
+const areEqual2 = string1.toLowerCase() === string3.toLowerCase();
+console.log(areEqual1);
+console.log(areEqual2);
