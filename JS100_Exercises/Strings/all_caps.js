cd819afd763c82b059f74962ca01a530ce04d724
Mojo Jojo <mojo@jojo.com>
2025-11-02 09:14:42 +0000

    added 2nd exercise.

---
+++ JS100_Exercises/Strings/all_caps.js
@@ -0,0 +1,5 @@
+// Take the string 'confetti floating everywhere' and transform it to upper case.
+
+const str = 'confetti floating everywhere';
+const strUpper = str.toUpperCase();
+console.log(strUpper);
