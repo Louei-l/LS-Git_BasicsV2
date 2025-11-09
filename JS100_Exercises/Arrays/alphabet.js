ee93e6c299d8667218041d0417caf63fe9aa3e73
Mojo Jojo <mojo@jojo.com>
2025-11-09 09:41:14 +0000

    added 4th exercise.

---
+++ JS100_Exercises/Arrays/alphabet.js
@@ -0,0 +1,7 @@
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+//Split the string alphabet into an array of characters.
+const arrayOfChars = alphabet.split('');
+
+//verify result
+console.log(arrayOfChars);
