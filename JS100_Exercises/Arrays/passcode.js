6e880dfa56ebac9a655b75811779452dcda43673
Mojo Jojo <mojo@jojo.com>
2025-11-09 12:56:10 +0000

    added 10th exercise.

---
+++ JS100_Exercises/Arrays/passcode.js
@@ -0,0 +1,7 @@
+const passcode = ['11', 'jZ5', 'hQ3f*', '8!7g3', 'p3Fs'];
+
+const joinedPasscode = passcode.join('-');
+
+//verify output
+console.log(joinedPasscode);
+console.log(typeof joinedPasscode);
