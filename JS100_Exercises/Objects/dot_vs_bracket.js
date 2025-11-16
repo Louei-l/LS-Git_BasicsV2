3041330f9de8aa65b7305d51d36e07f17ff6ef54
Mojo Jojo <mojo@jojo.com>
2025-11-16 12:25:42 +0000

    added 5th exercise.

---
+++ JS100_Exercises/Objects/dot_vs_bracket.js
@@ -0,0 +1,19 @@
+/*
+
+let ocean = {}; // we create an empty object called ocean
+let prefix = 'Indian'; // we create a varibale called prefix that hold the value of 'Indian'
+
+ocean.prefix = 'Pacific'; // inside ocean object we create a new property called prefix and assign it value of 'Pacific'
+
+console.log(ocean); // prefix: 'Pacific'
+
+
+
+let ocean = {}; // we create an empty object called ocean
+let prefix = 'Indian'; // we create a varibale called prefix that hold the value of 'Indian'
+
+ocean[prefix] = 'Pacific'; // inside ocean object we create a new property called 'Indian' since we refere to it using the bracket notation and then we assign it value of 'Pacific'
+
+console.log(ocean); // 'Indian': 'Pacific'
+
+*/
