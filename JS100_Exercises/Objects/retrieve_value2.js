1230fac3db210f649c2662643e037fe5b7258982
Mojo Jojo <mojo@jojo.com>
2025-11-16 17:20:24 +0000

    8th exercise.

---
+++ JS100_Exercises/Objects/retrieve_value2.js
@@ -0,0 +1,15 @@
+let jane = {
+  firstName: 'Jane',
+  lastName: 'Harrelson',
+  age: 32,
+  location: {
+    country: 'Denmark',
+    city: 'Aarhus'
+  },
+  occupation: 'engineer',
+};
+
+let JanesCountry = jane.location.country;
+
+//verify output
+console.log(JanesCountry);
