ff376c8771942a2c128dc6cd0beaf2a7ed435044
Mojo Jojo <mojo@jojo.com>
2025-11-16 12:48:24 +0000

    added 7th exercise.

---
+++ JS100_Exercises/Objects/car_keys.js
@@ -0,0 +1,12 @@
+let vehicle = {
+  manufacturer: 'Tesla',
+  model: 'Model X',
+  year: 2015,
+  range: 295,
+  seats: 7
+};
+
+const keys = Object.keys(vehicle);
+
+//verify output
+console.log(carKeys);
