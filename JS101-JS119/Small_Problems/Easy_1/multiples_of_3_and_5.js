bb1a12ce9b9c4757b469339b3b590f0bce8c7f48
Mojo Jojo <mojo@jojo.com>
2025-12-03 17:06:55 +0000

    added 10th exercise.

---
+++ JS101-JS119/Small_Problems/Easy_1/multiples_of_3_and_5.js
@@ -0,0 +1,24 @@
+/**
+ * Write a function that computes the sum of all numbers between 1 and some other number, 
+ * inclusive, that are multiples of 3 or 5. For instance, if the supplied number is 20, 
+ * the result should be 98 (3 + 5 + 6 + 9 + 10 + 12 + 15 + 18 + 20).
+
+ * You may assume that the number passed in is an integer greater than 1.
+ */
+
+function multisum(num) {
+  let sum = 0;
+  for (let i = 1; i <= num; i += 1) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum = sum + i;
+    }
+  }
+  return sum;
+}
+
+// verify output
+console.log(multisum(20));
+console.log(multisum(3));
+console.log(multisum(5));
+console.log(multisum(10));
+console.log(multisum(1000));
