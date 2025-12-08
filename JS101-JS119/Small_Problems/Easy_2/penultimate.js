07524895c0db75563d8b57401243cca98823d226
Mojo Jojo <mojo@jojo.com>
2025-12-08 17:20:12 +0000

    added 6th exercise.

---
+++ JS101-JS119/Small_Problems/Easy_2/penultimate.js
@@ -0,0 +1,21 @@
+/**
+ * Write a function that returns the next to last word in the String passed to it as an argument.
+ * Words are any sequence of non-blank characters.
+ * You may assume that the input String will always contain at least two words.
+ * 
+ * Examples:
+ * console.log(penultimate("last word") === "last"); // logs true
+ * console.log(penultimate("Launch School is great!") === "is"); // logs true
+ */
+
+function penultimate(text) {
+  if (typeof text !== 'string') return;
+  let convertTextToArray = text.split(' ');
+  let indexBeforeLast = convertTextToArray.length -2;
+  let pinultimateWord = convertTextToArray[indexBeforeLast]
+  return pinultimateWord;
+}
+
+//verify output
+console.log(penultimate("last word") === "last"); // logs true
+console.log(penultimate("Launch School is great!") === "is"); // logs true
