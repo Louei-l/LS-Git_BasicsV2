583e5a3ee73ee2adf9835f920df148e86a12749d
Mojo Jojo <mojo@jojo.com>
2025-12-13 11:59:25 +0000

    added 9th exercise.

---
+++ JS101-JS119/Small_Problems/Easy_2/how_old_is_teddy.js
@@ -0,0 +1,13 @@
+/**
+ * Build a program that randomly generates Teddy's age, and logs it to the console. 
+ * Have the age be a random number between 20 and 120 (inclusive).
+
+Example Output:
+ */
+
+teddyAge = () => {
+  const age =  Math.floor(Math.random() * (max - min + 1)) + min;
+  console.log(`Teddy is ${age} years old!`);
+}
+
+teddyAge();
